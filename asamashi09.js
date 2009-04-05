75262c9469199c2590818e4ba043609ede063b33
Kyo Nagashima <kyo@hail2u.net>
2009-04-05 12:16:14 +0900

    clean-up code

--- asamashi09.js
+++ asamashi09.js
@@ -8,14 +8,13 @@ function init() {
   });
 
   if (location.hash.match(/^#([a-zA-Z0-9]{10}):(.+?):?(http:\/\/.+)?$/)) {
-    $('#asinCode').val()    = (RegExp.$1 ? RegExp.$1 : '');
-    $('#associateId').val() = (RegExp.$2 ? RegExp.$2 : 'hail2unet-22');
-    $('#templateUrl').val() = (RegExp.$3 ? decodeURIComponent(RegExp.$3) : 'http://labs.hail2u.net/amazon/asamashi/template.js');
+    $('#asinCode').val((RegExp.$1 ? RegExp.$1 : ''));
+    $('#associateId').val((RegExp.$2 ? RegExp.$2 : 'hail2unet-22'));
+    $('#templateUrl').val((RegExp.$3 ? decodeURIComponent(RegExp.$3) : 'http://labs.hail2u.net/amazon/asamashi/template.js'));
     loadTmpl();
   }
 
-  $('#asinCode').focus();
-  $('#asinCode').select();
+  $('#asinCode').focus().select();
 }
 
 function loadTmpl() {
@@ -24,35 +23,18 @@ function loadTmpl() {
   var tmplUrl      = $('#templateUrl').val();
 
   if (!asin) {
-    $('<p/>').append(
-      'フォーム入力エラー: ASINコードが指定されていません。'
-    ).appendTo('#result');
-    $('#asinCode').focus();
-    $('#asinCode').select();
-    return false;
+    $('<p/>').append('フォーム入力エラー: ASINコードが指定されていません。').appendTo('#result');
+    $('#asinCode').focus().select();
+  } else if (!associate_id) {
+    $('<p/>').append('フォーム入力エラー: アソシエイトIDが指定されていません。').appendTo('#result');
+    $('#associateId').focus().select();
+  } else if (!tmplUrl) {
+    $('<p/>').append('フォーム入力エラー: テンプレートURLが指定されていません。').appendTo('#result');
+    $('#templateUrl').focus().select();
+  } else {
+    var template = '';
+    doSearch(asin, associate_id, template);
   }
-
-  if (!associate_id) {
-    $('<p/>').append(
-      'フォーム入力エラー: アソシエイトIDが指定されていません。'
-    ).appendTo('#result');
-    $('#associateId').focus();
-    $('#associateId').select();
-    return false;
-  }
-
-  if (!tmplUrl) {
-    $('<p/>').append(
-      'フォーム入力エラー: テンプレートURLが指定されていません。'
-    ).appendTo('#result');
-    $('#templateUrl').focus();
-    $('#templateUrl').select();
-    return false;
-  }
-
-  var template = ''; // dummy
-
-  doSearch(asin, associate_id, template);
 }
 
 function doSearch(asin, associate_id, template) {
@@ -62,42 +44,30 @@ function doSearch(asin, associate_id, template) {
     asin:        asin,
     tracking_id: associate_id
   }, function (data) {
-    with (data.value.items[0]) {
-      $('#result').empty();
+    var res = data.value.items[0];
+    $('#result').empty();
 
-      if (Items.Request.Errors) {
-        with (Items.Request.Errors.Error) {
-          $('<p/>').append(
-            document.createTextNode(Code + ': ' + Message)
-          ).appendTo('#result');
-        }
-      } else {
-        with (Items.Item) {
-          // アサマシプレビュー
-          $('<p/>').append(
-            $('<a/>').attr({
-              href: DetailPageURL
-            }).append(
-              $('<img/>').attr({
-                src:    MediumImage.URL,
-                width:  MediumImage.Width.content,
-                height: MediumImage.Height.content,
-              })
-            ).append(
-              document.createTextNode(ItemAttributes.Title)
-            )
-          ).appendTo('#result');
-          // アサマシコード
-          $('<textarea/>').attr({
-            cols: 80,
-            rows: 20
-          }).focus(function () {
-            this.select();
-          }).append(
-            document.createTextNode($('#result').html())
-          ).appendTo('#result');
-        }
-      }
+    if (res.Items.Request.Errors) {
+      var err = res.Items.Request.Errors.Error;
+      $('<p/>').append(document.createTextNode(err.Code + ': ' + err.Message)).appendTo('#result');
+    } else {
+      var item = res.Items.Item;
+      // アサマシプレビュー
+      $('<p/>').append($('<a/>').attr({
+        href: item.DetailPageURL
+      }).append($('<img/>').attr({
+        src:    item.MediumImage.URL,
+        width:  item.MediumImage.Width.content,
+        height: item.MediumImage.Height.content
+      })).append(document.createTextNode(item.ItemAttributes.Title))).appendTo('#result');
+
+      // アサマシコード
+      $('<p/>').append($('<textarea/>').attr({
+        cols: 80,
+        rows: 20
+      }).focus(function () {
+        this.select();
+      }).append(document.createTextNode($('#result').html()))).appendTo('#result');
     }
   });
 }
